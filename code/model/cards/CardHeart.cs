5590881449f43719121832a58f4e773d60e16d2c
antonliberg <antonliberg93@gmail.com>
2023-09-16 18:36:02 +0200

    .

--- code/model/cards/CardHeart.cs
+++ code/model/cards/CardHeart.cs
@@ -1,18 +1,16 @@
 using Godot;
-using System;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 
 public partial class CardHeart : Control
 {
 	public int MaxNrOfDice { get; private set; } = 20;
 	public int MinNrOfDice { get; private set; } = 1;
-    public bool IsRoomForDice() 
-        => LogicCard
-                .GetDice(this)
-                .Count <= MaxNrOfDice;
+	public bool IsRoomForDice() 
+		=> LogicCard
+				.GetDice(this)
+				.Count <= MaxNrOfDice;
 
-    public bool HasDice()
+	public bool HasDice()
 		=> LogicCard
 			.GetDice(this)
 			.Any();
